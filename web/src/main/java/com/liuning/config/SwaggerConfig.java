88d7b7b8ea338d3fe7caeda07b63b6433a56634c
liuningchuan <mayrzlmayrzl@outlook.com>
2019-09-23 21:09:35 +0800

    delete useless import

--- web/src/main/java/com/liuning/config/SwaggerConfig.java
+++ web/src/main/java/com/liuning/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.liuning.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
