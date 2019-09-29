ad194ae98745ff64d6fb09e466707c3ea1bb67b7
liuningchuan <mayrzlmayrzl@outlook.com>
2019-09-29 23:24:03 +0800

    update AppException

--- web/src/main/java/com/liuning/exception/AppException.java
+++ web/src/main/java/com/liuning/exception/AppException.java
@@ -1,7 +1,19 @@
 package com.liuning.exception;
 
-public class AppException {
+public class AppException extends RuntimeException{
 
+    /**
+     * 异常码
+     */
     private String code;
+
+    /**
+     * 异常信息
+     */
     private String message;
+
+    public AppException(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
