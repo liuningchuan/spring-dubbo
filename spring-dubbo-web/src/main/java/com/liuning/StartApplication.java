1692c0a4f2c2319e8c5d89ddf2da2e8058346d77
liuningchuan <mayrzlmayrzl@outlook.com>
2019-10-23 22:51:17 +0800

    rename module

--- spring-dubbo-web/src/main/java/com/liuning/StartApplication.java
+++ spring-dubbo-web/src/main/java/com/liuning/StartApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:dubbo-consumer.xml")
 public class StartApplication {
 
     public static void main(String[] args) {
