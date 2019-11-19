a90ee18976fb4a73d7f5f272d24e7120bca81913
liunzy <mayrzlmayrzl@outlook.com>
2019-11-19 22:47:08 +0800

    update DubboConfig

--- spring-dubbo-web/src/main/java/com/liuning/config/DubboConfig.java
+++ spring-dubbo-web/src/main/java/com/liuning/config/DubboConfig.java
@@ -11,26 +11,43 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DubboConfig {
 
+    /**
+     * zookeeper注册中心地址
+     */
     @Value("${dubbo.register.address}")
     private String dubboRegisterAdress;
-
+    /**
+     * 消费者超时时间
+     */
     @Value("${dubbo.consumer.timeout:30000}")
     private Integer consumerTimeout;
     @Value("${dubbo.consumer.retries:-1}")
     private Integer consumerRetries;
-
+    /**
+     * 生产者超时时间
+     */
     @Value("${dubbo.provider.timeout:30000}")
     private Integer providerTimeout;
-
+    /**
+     * 默认服务使用Dubbo服务
+     */
     @Value("${dubbo.protocol.name:dubbo}")
     private String protocolName;
+    /**
+     * 在20881端口监听服务
+     */
     @Value("${dubbo.protocol.port:20881}")
     private Integer protocolPort;
+    /**
+     * 线程池类型，默认fixed
+     */
     @Value("${dubbo.protocol.threadPool:fixed}")
     private String protocolThreadPool;
+    /**
+     * 线程池大小，默认200
+     */
     @Value("${dubbo.protocol.threads:200}")
     private Integer protocolThreads;
-
 
     @Bean
     public ApplicationConfig applicationConfig() {
