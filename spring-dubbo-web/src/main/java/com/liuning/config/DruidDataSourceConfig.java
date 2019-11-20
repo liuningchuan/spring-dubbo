1bfe2c17b16d6943b057a291858e569280ce2952
lnlynn <mayrzlmayrzl@outlook.com>
2019-11-20 23:54:36 +0800

    在日志中打印sql

--- spring-dubbo-web/src/main/java/com/liuning/config/DruidDataSourceConfig.java
+++ spring-dubbo-web/src/main/java/com/liuning/config/DruidDataSourceConfig.java
@@ -1,7 +1,6 @@
 package com.liuning.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
