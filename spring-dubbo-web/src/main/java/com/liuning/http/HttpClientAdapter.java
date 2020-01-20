d5294750698774c72c5f35b4a679d6dfaeb396ce
lnlynn <mayrzlmayrzl@outlook.com>
2020-01-20 23:20:39 +0800

    add HttpClientAdapter

---
+++ spring-dubbo-web/src/main/java/com/liuning/http/HttpClientAdapter.java
@@ -0,0 +1,59 @@
+package com.liuning.http;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * HttpClient封装
+ *
+ * @author liuning
+ * @since 2020-01-20
+ */
+@Component
+public class HttpClientAdapter {
+
+    /**
+     * GET请求
+     *
+     * @param url url
+     */
+    public void get(String url) {
+
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response;
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            // 配置信息
+            RequestConfig requestConfig = RequestConfig.custom()
+                    // 设置连接超时时间(单位毫秒)
+                    .setConnectTimeout(5000)
+                    // 设置请求超时时间(单位毫秒)
+                    .setConnectionRequestTimeout(5000)
+                    // socket读写超时时间(单位毫秒)
+                    .setSocketTimeout(5000)
+                    // 设置是否允许重定向(默认为true)
+                    .setRedirectsEnabled(true).build();
+
+            // 将上面的配置信息 运用到这个Get请求里
+            httpGet.setConfig(requestConfig);
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ spring-dubbo-web/src/test/java/com/liuning/test/HttpClientTest.java
@@ -0,0 +1,22 @@
+package com.liuning.test;
+
+import com.liuning.StartApplication;
+import com.liuning.http.HttpClientAdapter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {StartApplication.class})
+public class HttpClientTest {
+
+    @Autowired
+    HttpClientAdapter httpClientAdapter;
+
+    @Test
+    public void doGet() {
+        httpClientAdapter.get("http://127.0.0.1:8080/hello");
+    }
+}
