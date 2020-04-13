2b671ca4d8747610ed893360365e2150f168458d
lnlynn <mayrzlmayrzl@outlook.com>
2020-04-13 23:53:43 +0800

    complete httpclient

--- spring-dubbo-web/src/main/java/com/liuning/http/HttpClientAdapter.java
+++ spring-dubbo-web/src/main/java/com/liuning/http/HttpClientAdapter.java
@@ -1,13 +1,22 @@
 package com.liuning.http;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * HttpClient封装
@@ -17,6 +26,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class HttpClientAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpClientAdapter.class);
 
     /**
      * GET请求
@@ -28,7 +39,7 @@ public class HttpClientAdapter {
         HttpGet httpGet = new HttpGet(url);
         CloseableHttpResponse response;
 
-        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             // 配置信息
             RequestConfig requestConfig = RequestConfig.custom()
                     // 设置连接超时时间(单位毫秒)
@@ -47,13 +58,37 @@ public class HttpClientAdapter {
             // 从响应模型中获取响应实体
             HttpEntity responseEntity = response.getEntity();
 
-            System.out.println("响应状态为:" + response.getStatusLine());
+            logger.info("Response Status is" + response.getStatusLine());
             if (responseEntity != null) {
-                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
-                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+                logger.info("Response Content Length :" + responseEntity.getContentLength());
+                logger.info("Response Content :" + EntityUtils.toString(responseEntity));
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Httpclient Get error" + e);
+        }
+    }
+
+    /**
+     * post请求
+     */
+    public void post() {
+        HttpPost httpPost = new HttpPost("http://locahost:8080");
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            List<NameValuePair> formparams = new ArrayList<>();
+            formparams.add(new BasicNameValuePair("username","name"));
+            formparams.add(new BasicNameValuePair("password","word"));
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams,"UTF-8");
+            httpPost.setEntity(entity);
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+
+            HttpEntity responseEntity = response.getEntity();
+            logger.info("Response Status is" + response.getStatusLine());
+            if (responseEntity != null) {
+                logger.info("Response Content Length :" + responseEntity.getContentLength());
+                logger.info("Response Content :" + EntityUtils.toString(responseEntity));
+            }
+        } catch (Exception e) {
+            logger.error("HttpClient Post error");
         }
     }
 }
