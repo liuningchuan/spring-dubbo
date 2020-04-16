c14f35624b023e830426fbc6e8a07743c6a63a12
lnlynn <mayrzlmayrzl@outlook.com>
2020-04-17 00:00:36 +0800

    update HttpClientPool

--- spring-dubbo-web/src/main/java/com/liuning/http/HttpClientPool.java
+++ spring-dubbo-web/src/main/java/com/liuning/http/HttpClientPool.java
@@ -7,7 +7,6 @@ import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
@@ -15,11 +14,9 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.protocol.HttpContext;
 
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLHandshakeException;
-import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.UnknownHostException;
 
@@ -27,8 +24,15 @@ public class HttpClientPool {
 
     private static CloseableHttpClient httpClient = null;
 
-    static final int maxTotal= 20;//总最大连接数
-    static final int defaultMaxPerRoute= 20;//每条线路最大连接数 = 本系统核心线程数 , 这样永远不会超过最大连接
+    /**
+     * 总最大连接数
+     */
+    static final int maxTotal= 2;
+
+    /**
+     * 每条线路最大连接数 = 本系统核心线程数 , 这样永远不会超过最大连接
+     */
+    static final int defaultMaxPerRoute= 20;
 
     public static CloseableHttpClient getHttpClient() {
         if (null == httpClient) {
@@ -43,12 +47,12 @@ public class HttpClientPool {
 
     private static CloseableHttpClient init() {
 
-
         // 设置连接池
         ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
         LayeredConnectionSocketFactory sslsf = SSLConnectionSocketFactory.getSocketFactory();
         Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("http", plainsf).register("https", sslsf).build();
+                .register("http", plainsf).register("https", sslsf)
+                .build();
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
         // 配置最大连接数
         cm.setMaxTotal(maxTotal);
@@ -56,45 +60,41 @@ public class HttpClientPool {
         cm.setDefaultMaxPerRoute(defaultMaxPerRoute);
 
         // 请求重试处理
-        HttpRequestRetryHandler httpRequestRetryHandler = new HttpRequestRetryHandler() {
-            @Override
-            public boolean retryRequest(IOException exception,
-                                        int executionCount, HttpContext context) {
-                if (executionCount >= 2) {// 如果已经重试了2次，就放弃
-                    return false;
-                }
-                if (exception instanceof NoHttpResponseException) {// 如果服务器丢掉了连接，那么就重试
-                    return true;
-                }
-                if (exception instanceof SSLHandshakeException) {// 不要重试SSL握手异常
-                    return false;
-                }
-                if (exception instanceof InterruptedIOException) {// 超时
-                    return false;
-                }
-                if (exception instanceof UnknownHostException) {// 目标服务器不可达
-                    return false;
-                }
-                if (exception instanceof SSLException) {// SSL握手异常
-                    return false;
-                }
-
-                HttpClientContext clientContext = HttpClientContext.adapt(context);
-                HttpRequest request = clientContext.getRequest();
-
-                return !(request instanceof HttpEntityEnclosingRequest);
+        HttpRequestRetryHandler httpRequestRetryHandler = (exception, executionCount, context) -> {
+            // 如果已经重试了2次，就放弃
+            if (executionCount >= 2) {
+                return false;
+            }
+            // 如果服务器丢掉了连接，那么就重试
+            if (exception instanceof NoHttpResponseException) {
+                return true;
+            }
+            // 不要重试SSL握手异常
+            if (exception instanceof SSLHandshakeException) {
+                return false;
+            }
+            // 超时
+            if (exception instanceof InterruptedIOException) {
+                return false;
+            }
+            // 目标服务器不可达
+            if (exception instanceof UnknownHostException) {
+                return false;
+            }
+            // SSL握手异常
+            if (exception instanceof SSLException) {
+                return false;
             }
 
+            HttpClientContext clientContext = HttpClientContext.adapt(context);
+            HttpRequest request = clientContext.getRequest();
+
+            return !(request instanceof HttpEntityEnclosingRequest);
         };
 
-        CloseableHttpClient newHttpclient;
-
-        newHttpclient = HttpClients.custom()
-                .setConnectionManager(cm)
+        return HttpClients.custom().setConnectionManager(cm)
                 .setRetryHandler(httpRequestRetryHandler)
                 .build();
-
-        return newHttpclient;
     }
 
 }
