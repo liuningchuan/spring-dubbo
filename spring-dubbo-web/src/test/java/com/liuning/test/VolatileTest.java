a628ba4efa10a251d9b1086873504fc8da183d5d
lnlynn <mayrzlmayrzl@outlook.com>
2020-06-04 00:19:22 +0800

    AtomicInteger.getAndIncrement()

--- spring-dubbo-web/src/test/java/com/liuning/test/VolatileTest.java
+++ spring-dubbo-web/src/test/java/com/liuning/test/VolatileTest.java
@@ -1,6 +1,7 @@
 package com.liuning.test;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * volatile变量自增测试
@@ -11,28 +12,37 @@ public class VolatileTest {
 
     private static final int THREADS_COUNT = 20;
 
+    /**
+     * 最开始的版本在IDEA的run模式下会死循环，使用CountDownLatch解决了这个问题
+     */
     private static CountDownLatch countDownLatch = new CountDownLatch(THREADS_COUNT);
 
-    private static void increase() {
+    /**
+     * AtomicInteger.getAndIncrement是典型的CAS算法
+     * getAndAddInt方法解析：拿到内存位置的最新值v，使用CAS尝试修将内存位置的值修改为目标值v+delta，
+     *   如果修改失败，则获取该内存位置的新值v，然后继续尝试，直至修改成功。
+     */
+    public static AtomicInteger number = new AtomicInteger(0);
+
+    private static  void increase() {
         //race++不是原子操作
         race++;
+        number.getAndIncrement();
     }
 
     public static void main(String[] args) throws InterruptedException {
         Thread[] threads = new Thread[THREADS_COUNT];
         for (int i = 0; i < THREADS_COUNT; i++) {
             threads[i] = new Thread(() -> {
-                for (int i1 = 0; i1 < 10000; i1++) {
+                for (int j = 0; j < 10000; j++) {
                     increase();
                 }
                 countDownLatch.countDown();
             });
             threads[i].start();
         }
-        while (Thread.activeCount() > 1) {
-            Thread.yield();
-        }
         countDownLatch.await();
         System.out.println(race);
+        System.out.println(number);
     }
 }
