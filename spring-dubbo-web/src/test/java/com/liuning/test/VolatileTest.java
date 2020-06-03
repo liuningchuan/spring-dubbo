534eae93623110a53a07df31b7cbc84395b3e529
lnlynn <mayrzlmayrzl@outlook.com>
2020-06-03 23:59:51 +0800

    update test

--- spring-dubbo-web/src/test/java/com/liuning/test/VolatileTest.java
+++ spring-dubbo-web/src/test/java/com/liuning/test/VolatileTest.java
@@ -1,5 +1,7 @@
 package com.liuning.test;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * volatile变量自增测试
  */
@@ -7,26 +9,30 @@ public class VolatileTest {
 
     private static volatile int race = 0;
 
+    private static final int THREADS_COUNT = 20;
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(THREADS_COUNT);
+
     private static void increase() {
         //race++不是原子操作
         race++;
     }
 
-    private static final int THREADS_COUNT = 20;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread[] threads = new Thread[THREADS_COUNT];
         for (int i = 0; i < THREADS_COUNT; i++) {
             threads[i] = new Thread(() -> {
                 for (int i1 = 0; i1 < 10000; i1++) {
                     increase();
                 }
+                countDownLatch.countDown();
             });
             threads[i].start();
         }
-        while (Thread.activeCount() < 0) {
+        while (Thread.activeCount() > 1) {
             Thread.yield();
         }
+        countDownLatch.await();
         System.out.println(race);
     }
 }
