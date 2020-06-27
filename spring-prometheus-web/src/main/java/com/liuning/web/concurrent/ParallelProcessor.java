a0d107dcc4a4320795013d304179385a1ea48219
lnlynn <mayrzlmayrzl@outlook.com>
2020-06-27 23:27:19 +0800

    添加并行处理类

---
+++ spring-prometheus-web/src/main/java/com/liuning/web/concurrent/ParallelProcessor.java
@@ -0,0 +1,59 @@
+package com.liuning.web.concurrent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+/**
+ * @author: liuning
+ * @description: 并行处理类
+ * @create: 2020-06-27 21:37
+ * @version: 1.0
+ */
+public class ParallelProcessor<T> {
+
+    private static final Logger log = LoggerFactory.getLogger(ParallelProcessor.class);
+    private static ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(10);
+    private Map<String, Consumer<T>> checkers;
+    private T context;
+
+    public ParallelProcessor(Map<String, Consumer<T>> checkers, T context) {
+        this.checkers = checkers;
+        this.context = context;
+    }
+
+    public ParallelProcessor<T> addChecker(String desc, Consumer<T> checker) {
+        checkers.put(desc, checker);
+        return this;
+    }
+
+    public void executeAll() {
+        try {
+            long start = System.currentTimeMillis();
+            CountDownLatch countDownLatch = new CountDownLatch(checkers.size());
+
+            checkers.keySet().forEach(desc -> {
+                newFixedThreadPool.execute(() -> {
+                    log.info("开始进行校验：{}", desc);
+                    checkers.get(desc).accept(context);
+                    log.info("完成执行校验：{}", desc);
+                });
+                countDownLatch.countDown();}
+            );
+            countDownLatch.await();
+            long cost = System.currentTimeMillis() - start;
+            log.info("cost is :" + cost);
+        } catch (InterruptedException e) {
+            log.error("线程中断",e);
+            Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            log.error("并行校验发生异常，", e);
+            throw e;
+        }
+    }
+}
--- spring-prometheus-web/src/main/java/com/liuning/web/controller/FreemarkerController.java
+++ spring-prometheus-web/src/main/java/com/liuning/web/controller/FreemarkerController.java
@@ -1,16 +1,20 @@
 package com.liuning.web.controller;
 
+import com.liuning.common.enums.ErrorCode;
+import com.liuning.common.exception.AppException;
 import com.liuning.common.utils.ValidationUtils;
 import com.liuning.dto.Request;
 import com.liuning.dto.Result;
 import com.liuning.dto.trade.TradeDetailQueryReqDto;
 import com.liuning.dto.trade.TradeDetailQueryResDto;
 import com.liuning.web.aspect.BusiLog;
+import com.liuning.web.concurrent.ParallelProcessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
 @RestController
 class FreemarkerController {
@@ -25,8 +29,36 @@ class FreemarkerController {
     @PostMapping("/trade/detail")
     @BusiLog(name = "交易详情查询接口", ignore = true)
     public Result<TradeDetailQueryResDto> tradeDetailQuery(@RequestBody Request<TradeDetailQueryReqDto> req) {
+
+        //同事写的一个不可取的并行校验方法
+        ParallelProcessor<Request<TradeDetailQueryReqDto>> parallelProcessor =
+                new ParallelProcessor<>(new HashMap<>(),req);
+        parallelProcessor.addChecker("校验服务", this::checkService)
+                .addChecker("校验日志流水号",this::checkClientSrial)
+                .addChecker("校验交易流水号", this::checkTradeNo)
+                .executeAll();
+
         ValidationUtils.validate(req);
         System.out.println(req.getData().getTradeNo());
         return Result.success();
     }
+
+    private void checkService(Request<TradeDetailQueryReqDto> tradeDetailQueryReqDtoRequest) {
+        if (!tradeDetailQueryReqDtoRequest.getService().equals("trade")) {
+            throw new AppException(ErrorCode.PARAM_ERROR.getCode(), ErrorCode.PARAM_ERROR.getMessage());
+        }
+    }
+
+    private void checkClientSrial(Request<TradeDetailQueryReqDto> tradeDetailQueryReqDtoRequest) {
+        if (!tradeDetailQueryReqDtoRequest.getClientSerialNo().equals("trade")) {
+            throw new AppException(ErrorCode.PARAM_ERROR.getCode(), ErrorCode.PARAM_ERROR.getMessage());
+        }
+    }
+
+    private void checkTradeNo(Request<TradeDetailQueryReqDto> tradeDetailQueryReqDtoRequest) {
+        if (!tradeDetailQueryReqDtoRequest.getData().getTradeNo().equals("trade")) {
+            throw new AppException(ErrorCode.PARAM_ERROR.getCode(), ErrorCode.PARAM_ERROR.getMessage());
+        }
+    }
+
 }
