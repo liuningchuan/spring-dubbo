73ce3e3a6c91dc6e012e9224623ecb851d86fc6d
lnlynn <mayrzlmayrzl@outlook.com>
2020-07-04 23:57:12 +0800

    add IdGenerator

---
+++ spring-prometheus-common/src/main/java/com/liuning/common/utils/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.liuning.common.utils;
+
+/**
+ * @author: liuning
+ * @description: id生成器
+ * @create: 2020-07-04 23:16
+ * @version: 1.0
+ */
+public interface IdGenerator {
+
+    /**
+     * 获取ID
+     * @return ID
+     */
+    long nextId();
+
+}
+
