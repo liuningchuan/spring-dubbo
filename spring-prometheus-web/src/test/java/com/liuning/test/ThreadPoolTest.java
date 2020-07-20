4bfd8ddee7d2d5e732f42bfc6d375f57ca507c7d
lnlynn <mayrzlmayrzl@outlook.com>
2020-07-20 23:55:42 +0800

    add ExecutorService

--- spring-prometheus-web/src/test/java/com/liuning/test/ThreadPoolTest.java
+++ spring-prometheus-web/src/test/java/com/liuning/test/ThreadPoolTest.java
@@ -3,6 +3,9 @@ package com.liuning.test;
 import com.liuning.web.concurrent.singleton.ThreadExecutor;
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author: liuning
  * @description: 线程池测试
@@ -12,8 +15,28 @@ import org.junit.Test;
 public class ThreadPoolTest {
 
     @Test
-    public void threadPoolTest() {
+    public void threadPoolTest_1() {
         ThreadExecutor.execute(() -> System.out.println(Thread.currentThread().getName()));
         System.out.println(Thread.currentThread().getName());
+    }
+
+    @Test
+    public void threadPoolTest_2() {
+
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i=0; i < 100; i++) {
+            cachedThreadPool.submit(() -> System.out.println(Thread.currentThread().getName()));
+        }
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+        for (int i=0; i < 100; i++) {
+            fixedThreadPool.submit(() -> System.out.println(Thread.currentThread().getName()));
+        }
+
+        ExecutorService singleaThreadPool = Executors.newSingleThreadExecutor();
+        for (int i=0; i < 100; i++) {
+            singleaThreadPool.submit(() -> System.out.println(Thread.currentThread().getName()));
+        }
+
     }
 }
