a43b6e6a3fe0e5f7738c6c848c243356e1ed0108
lnlynn <mayrzlmayrzl@outlook.com>
2020-07-27 00:03:45 +0800

    测试Gson自定义序列化Date类型

--- spring-prometheus-web/src/test/java/com/liuning/test/JSONTest.java
+++ spring-prometheus-web/src/test/java/com/liuning/test/JSONTest.java
@@ -4,12 +4,17 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.google.gson.*;
+import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.google.gson.reflect.TypeToken;
 import com.liuning.common.utils.JsonUtils;
 import com.liuning.dao.entity.User;
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.junit.Test;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.ParsePosition;
 import java.util.*;
 
 /**
@@ -56,17 +61,100 @@ public class JSONTest {
         String jsonStr = JSONObject.toJSONString(list);
         System.out.println(JSON.toJSONString(list));
 
-        System.out.println("TypeReference Test begin.");
-
+        //TypeReference
         Type type = new TypeReference<List<User>>() {}.getType();
         List<User> userList = JSON.parseObject(jsonStr, type);
         for (User user : userList) {
             System.out.println(user);
         }
 
-        System.out.println("JSONArray Test begin.");
+        //JSONArray
+        String arrayString = "[{\"email\":\"JSONArray\",\"id\":0,\"name\":\"user0\",\"password\":\"password0\"}]";
+        JSONArray jsonArray = JSON.parseArray(arrayString);
+        System.out.println("size is : " + jsonArray.size() + " and content is : " + jsonArray.get(0));
 
-        JSONArray jsonArray = JSON.parseArray(jsonStr);
-        System.out.println(jsonArray);
+        //JSONObject
+        String str = "{\"email\":\"email0\",\"id\":0,\"name\":\"user0\",\"password\":\"password0\"}";
+        JSONObject jsonObject = JSON.parseObject(str);
+        System.out.println("email is : " + jsonObject.get("email"));
+    }
+
+    @Test
+    public void gsonTest() {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Date.class, (JsonSerializer<Date>) (date, type, jsonSerializationContext) ->
+                        new JsonPrimitive(FastDateFormat.getInstance("yyyyMMdd").format(date))
+                )
+                .registerTypeAdapter(Date.class, (JsonDeserializer<Date>) (jsonElement, type, jsonDeserializationContext) -> {
+                    JsonPrimitive jp = jsonElement.getAsJsonPrimitive();
+                    if (jp.isNumber()) {
+                        return new Date(jp.getAsLong());
+                    } else {
+                        String date = jp.getAsString();
+                        try {
+                            FastDateFormat.getInstance("yyyyMMdd").format(date);
+                        } catch (Exception ignore) {
+                        }
+                        try {
+                            return ISO8601Utils.parse(date, new ParsePosition(0));
+                        } catch (ParseException e) {
+                            throw new JsonSyntaxException(date, e);
+                        }
+                    }
+                })
+                .create();
+
+        DateExample date = new DateExample();
+        date.setName("liuning");
+        date.setDescription("刘宁");
+        date.setDate(new Date());
+        System.out.println(gson.toJson(date));
+
+        String dateString = "{\"name\":\"liuning\",\"description\":\"刘宁\",\"date\":\"20200727\"}";
+        DateExample dateExample = gson.fromJson(dateString, DateExample.class);
+        System.out.println("date is " + dateExample.getDate());
+
+        //第二种处理Date序列化的方式
+        Gson GSON = new GsonBuilder().setDateFormat("yyyyMMddhhmmss").create();
+        System.out.println("===========");
+        System.out.println(GSON.toJson(date));
+
+        String dateString2 = "{\"name\":\"liuning\",\"description\":\"刘宁\",\"date\":\"20200727224140\"}";
+        DateExample dateExample2 = GSON.fromJson(dateString2, DateExample.class);
+        System.out.println("date is " + dateExample2.getDate());
+
+    }
+
+    private static class DateExample {
+
+        public String name;
+
+        public String description;
+
+        public Date date;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
     }
 }
