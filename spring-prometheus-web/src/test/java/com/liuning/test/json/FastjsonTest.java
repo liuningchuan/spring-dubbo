a14507c320a29e1435645a98b3d011f7f1b5ca77
lnlynn <mayrzlmayrzl@outlook.com>
2021-01-05 22:59:52 +0800

    feat：fastjson字符串转义符测试

--- spring-prometheus-web/src/test/java/com/liuning/test/json/FastjsonTest.java
+++ spring-prometheus-web/src/test/java/com/liuning/test/json/FastjsonTest.java
@@ -5,8 +5,14 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.liuning.dao.entity.User;
+import com.liuning.dao.mapper.UserMapper;
+import com.liuning.web.StartApplication;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +22,12 @@ import java.util.List;
  * @description Alibaba Fastjson test
  * @since 2020-08-06 23:57
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {StartApplication.class})
 public class FastjsonTest {
+
+    @Resource
+    private UserMapper userMapper;
 
     @Test
     public void fastJsonTest() {
@@ -49,6 +60,28 @@ public class FastjsonTest {
         String str = "{\"email\":\"email0\",\"id\":0,\"name\":\"user0\",\"password\":\"password0\"}";
         JSONObject jsonObject = JSON.parseObject(str);
         System.out.println("email is : " + jsonObject.get("email"));
+    }
+
+    @Test
+    public void fastJsonStringTest() {
+
+        User user = new User();
+        user.setId(1);
+        user.setName("user");
+        user.setEmail("email");
+        user.setPassword("password");
+
+        String jsonStr = JSONObject.toJSONString(user);
+        System.out.println(jsonStr);
+        System.out.println(JSON.toJSONString(jsonStr));
+        System.out.println(JSON.toJSONString(JSON.toJSONString(jsonStr)));
+
+        User addUser = new User();
+        addUser.setName("test");
+        addUser.setPassword(jsonStr);
+        addUser.setEmail(JSON.toJSONString(jsonStr));
+
+        userMapper.insertSelective(addUser);
     }
 
 }
