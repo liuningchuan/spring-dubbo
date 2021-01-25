1142034726e444e2392bcc315bc373a855d778b1
lnlynn <mayrzlmayrzl@outlook.com>
2021-01-25 23:54:44 +0800

    feat：添加注释

--- spring-prometheus-web/src/main/java/com/liuning/web/ServletInitializer.java
+++ spring-prometheus-web/src/main/java/com/liuning/web/ServletInitializer.java
@@ -3,8 +3,11 @@ package com.liuning.web;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 /**
- * SpringBootServletInitializer是一个支持
+ * SpringBootServletInitializer是一个支持，让应用在tomcat容器中以war包的形式运行
  * Spring Boot的Spring WebApplicationInitializer实现。
  *
  * @author LiuNing
@@ -16,5 +19,10 @@ public class ServletInitializer extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         System.setProperty("app-name", "spring-dubbo");
         return builder.sources(StartApplication.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
     }
 }
