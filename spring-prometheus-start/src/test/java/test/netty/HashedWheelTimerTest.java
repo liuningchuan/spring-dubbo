f0a0d683d5a9b96eff227d4a6f744070b6b09848
lnlynn <mayrzlmayrzl@outlook.com>
2021-05-14 23:44:23 +0800

    feat：新增时间轮测试

--- spring-prometheus-start/src/test/java/test/netty/HashedWheelTimerTest.java
+++ spring-prometheus-start/src/test/java/test/netty/HashedWheelTimerTest.java
@@ -20,9 +20,30 @@ public class HashedWheelTimerTest {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         HashedWheelTimer hashedWheelTimer = new HashedWheelTimer(100, TimeUnit.MILLISECONDS);
         System.out.println("start:" + LocalDateTime.now().format(formatter));
+
         hashedWheelTimer.newTimeout(timeout -> {
             System.out.println("task :" + LocalDateTime.now().format(formatter));
         }, 3, TimeUnit.SECONDS);
+
         Thread.sleep(5000);
+    }
+
+    @Test
+    public void test2() throws Exception {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        HashedWheelTimer hashedWheelTimer = new HashedWheelTimer(100, TimeUnit.MILLISECONDS);
+        System.out.println("start:" + LocalDateTime.now().format(formatter));
+
+        hashedWheelTimer.newTimeout(timeout -> {
+            Thread.sleep(3000);
+            System.out.println("task1:" + LocalDateTime.now().format(formatter));
+        }, 3, TimeUnit.SECONDS);
+
+        //时间轮是串行执行的，当前一个任务执行时间过长的时候，会影响后续任务的到期执行时间的。
+        hashedWheelTimer.newTimeout(timeout -> System.out.println("task2:" + LocalDateTime.now().format(
+                formatter)), 4, TimeUnit.SECONDS);
+
+        Thread.sleep(10000);
     }
 }
